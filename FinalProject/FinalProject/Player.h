7e03b09a5deee1bdc9cd6bee2faefeace52c90cd
kyee077 <katherineyee3@gmail.com>
2014-10-29 12:25:22 -0400

    Test

--- FinalProject/FinalProject/Player.h
+++ FinalProject/FinalProject/Player.h
@@ -2,9 +2,10 @@
 //  Player.h
 //  FinalProject
 //
-//  Created by Shawn Lamothe on 2014-10-29.
+//  Created by Shawn Lamothe and Katherine Yee on 2014-10-29.
 //  Copyright (c) 2014 shawnlamothe. All rights reserved.
 //
+// Student number: Katherine Yee 7233772
 
 #ifndef __FinalProject__Player__
 #define __FinalProject__Player__
