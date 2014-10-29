94e11c390532dbbb45f6aa31ec1d4e858a7dfe11
ShawnLamothe <slamo025@uottawa.cca>
2014-10-29 12:27:27 -0400

    test

--- FinalProject/FinalProject/Tile.h
+++ FinalProject/FinalProject/Tile.h
@@ -2,9 +2,10 @@
 //  Tile.h
 //  FinalProject
 //
-//  Created by Shawn Lamothe on 2014-10-28.
+//  Created by Shawn Lamothe and Katherine Yee on 2014-10-28.
 //  Copyright (c) 2014 shawnlamothe. All rights reserved.
 //
+//  Student number: Katherine Yee 7233772
 
 #ifndef __FinalProject__Tile__
 #define __FinalProject__Tile__
