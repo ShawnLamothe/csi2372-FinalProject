3798fc19b9b70eee6867441400d3df31400f1b4d
ShawnLamothe <slamo025@uottawa.cca>
2014-10-29 12:20:12 -0400

    added player class

---
+++ FinalProject/FinalProject/Player.cpp
@@ -0,0 +1,9 @@
+//
+//  Player.cpp
+//  FinalProject
+//
+//  Created by Shawn Lamothe on 2014-10-29.
+//  Copyright (c) 2014 shawnlamothe. All rights reserved.
+//
+
+#include "Player.h"
---
+++ FinalProject/FinalProject/Player.h
@@ -0,0 +1,14 @@
+//
+//  Player.h
+//  FinalProject
+//
+//  Created by Shawn Lamothe on 2014-10-29.
+//  Copyright (c) 2014 shawnlamothe. All rights reserved.
+//
+
+#ifndef __FinalProject__Player__
+#define __FinalProject__Player__
+
+#include <iostream>
+
+#endif /* defined(__FinalProject__Player__) */
